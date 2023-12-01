88160c2c58bdba14af22459dbc15c042a50879ee
Beezyboys <147273107+Beezyboys@users.noreply.github.com>
2023-12-01 00:31:23 -0600

    Fleshed out a good bit of the play game needs a few tweeks
    Will start to make branches when implementing new stuff like styling and then the quiz in that order quiz comes last
    I need it to look good

--- MagicProject/src/main/webapp/js/playController.js
+++ MagicProject/src/main/webapp/js/playController.js
@@ -6,28 +6,50 @@
 
 
 
-		magicapp.controller('playController', function($scope, $http, $routeParams, $location) {
+	magicapp.controller('playController', function($scope, $http, $routeParams, $location) {
 		$scope.winner = [true, false];
 		$scope.colors = ['W', 'U', 'B', 'R', 'G', 'WU', 'WB', 'WR', 'WG', 'UB', 'UR', 'UG', 'BR', 'BG', 'RG', 'WUB', 'WUR', 'WUG', 'WBR', 'WBG', 'WRG', 'UBR', 'UBG', 'URG', 'BRG', 'WUBR', 'WUBG', 'WURG', 'WBRG', 'UBRG', 'WUBRG', 'Colorless'];
-		/*
-		$scope.updatePlayer = function() {
-			$http.put("/magicproject/webapi/CommanderBattleBook/", $scope.player)
-				.then(function(response) {
-					$scope.updateStatus = 'update successful';
-				}, function(response) {
-					$scope.updateStatus = 'error trying to update game';
-					console.log('error http PUT player: ' + response.status);
-				});
-		}
-		*/
+
+		$scope.player1 = {
+			lifeTotal: 40,
+			commandDamageFromPlayer2: 0,
+			commandDamageFromPlayer3: 0,
+			commandDamageFromPlayer4: 0,
+			poisonCounter: 0
+		};
+		
+		$scope.player2 = {
+			lifeTotal: 40,
+			commandDamageFromPlayer1: 0,
+			commandDamageFromPlayer3: 0,
+			commandDamageFromPlayer4: 0,
+			poisonCounter: 0
+		};
+		
+		$scope.player3 = {
+			lifeTotal: 40,
+			commandDamageFromPlayer1: 0,
+			commandDamageFromPlayer2: 0,
+			commandDamageFromPlayer4: 0,
+			poisonCounter: 0
+		};
+		
+		$scope.player4 = {
+			lifeTotal: 40,
+			commandDamageFromPlayer1: 0,
+			commandDamageFromPlayer2: 0,
+			commandDamageFromPlayer3: 0,
+			poisonCounter: 0
+		};
+
 		$scope.getGameById = function() {
 			$http.get("/magicproject/webapi/CommanderBattleBook/game/" + $routeParams.gameId)
 				.then(function(response) {
 					var players = response.data;
-						$scope.player1 = players[0];
-						$scope.player2 = players[1];
-						$scope.player3 = players[2];
-						$scope.player4 = players[3];
+					$scope.player1 = players[0];
+					$scope.player2 = players[1];
+					$scope.player3 = players[2];
+					$scope.player4 = players[3];
 				}, function(response) {
 					console.log('error http GET game by id: ' + response.status);
 
